d80047c6625f804ba83fe97305cbeff1bb5b59a3
Germán Aliprandi <galiprandi@gmail.com>
2025-05-01 02:43:35 -0300

    feat: create UserAvatar component for consistent avatar rendering

---
+++ components/UserAvatar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import { LazyRender } from "@galiprandi/react-tools";
+import { CSSProperties } from "react";
+
+export default function UserAvatar({
+  src,
+  alt,
+  size = 55,
+}: {
+  src: string;
+  alt: string;
+  size?: number;
+}) {
+  return (
+    <LazyRender placeholder={<Skeleton size={size} />}>
+      <Image src={src} alt={alt} width={size} height={size} style={styles} />
+    </LazyRender>
+  );
+}
+
+const Skeleton = ({ size = 55 }: { size?: number }) => {
+  return <div style={{ ...styles, width: size, height: size }} />;
+};
+
+const styles: CSSProperties = {
+  borderRadius: "50%",
+  backgroundColor: "#ccc",
+};
